688cdfd6cd5cccf5d07f33ed87314aa11e144553
salientype <jorge@salientype.com>
2020-01-17 14:36:09 -0500

    registering users route working

--- server.js
+++ server.js
@@ -16,18 +16,19 @@ var cookieParser = require('cookie-parser');
 
 const bcrypt = require('bcrypt');
 
-const Sequelize = require('sequelize')
-const GroupsModel = require('./models/groups')
+const Sequelize = require('sequelize');
+const GroupsModel = require('./models/groups');
+const UsersModel = require('./models/users');
 
 const connectionString = `postgres://${config.username}:${config.password}@${config.host}:${config.port}/${config.database}`
 const sequelize = new Sequelize(process.env.DATABASE_URL || connectionString, {
-    
+
     dialect: 'postgres',
     pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     }
 
 })
@@ -35,7 +36,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL || connectionString, {
 console.log(connectionString)
 
 const Groups = GroupsModel(sequelize, Sequelize);
-
+const Users = UsersModel(sequelize, Sequelize);
 
 var app = express();
 
@@ -46,32 +47,32 @@ app.use(cookieParser());
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/groups', function(req, res) {
+app.get('/groups', function (req, res) {
     const groups = Groups.findAll().then((results) => {
         res.render('pages/groups', { groups: results });
-    }).catch(function(e) {
+    }).catch(function (e) {
         return 'no results'
     })
 });
 
-app.get('/create_group', function(req, res) {
+app.get('/create_group', function (req, res) {
     res.render('pages/create_group');
 });
 
-app.get('/group_info/:id', function(req, res) {
+app.get('/group_info/:id', function (req, res) {
     let id = req.params.id;
     const group = Groups.findOne({ where: { id: id } }).then(results => {
         res.render('pages/group_info', { group: results });
-    }).catch(function(e) {
+    }).catch(function (e) {
         return 'no results'
     })
 });
 
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
     res.sendFile(__dirname + '/public/' + 'login.html');
 });
 
-app.get('/register', function(req, res) {
+app.get('/register', function (req, res) {
     res.sendFile(__dirname + '/public/' + 'register.html');
 });
 
@@ -79,7 +80,7 @@ app.get('/api/groups', function (req, res) {
     Groups.findAll().then((results) => {
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(results));
-    }).catch(function(e) {
+    }).catch(function (e) {
         console.log(e);
         res.status(434).send('error retrieving groups');
     })
@@ -96,7 +97,7 @@ app.post('/api/groups', function (req, res) {
     Groups.create(data).then(function (group) {
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(group));
-    }).catch(function(e) {
+    }).catch(function (e) {
         res.status(434).send('unable to create group')
     })
 });
@@ -110,12 +111,12 @@ app.post('/api/login', function (req, res) {
                 email: email
             },
         }).then((results) => {
-            bcrypt.compare(password, results.password).then(function(matched) {
+            bcrypt.compare(password, results.password).then(function (matched) {
                 if (matched) {
-                  req.session.user = results.id;
-                  req.session.name = results.name;
-                  res.setHeader('Content-Type', 'application/json');
-                  res.end(JSON.stringify(results));
+                    req.session.user = results.id;
+                    req.session.name = results.name;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify(results));
                 } else {
                     res.status(434).send('Email/Password combination did not match')
                 }
@@ -126,26 +127,36 @@ app.post('/api/login', function (req, res) {
     } else {
         res.status(434).send('Both email and password is required to login')
     }
-  });
-  
-  app.post('/api/register', function (req, res) {
-    console.log(req.body.email);
-  let data = {
-      name: req.body.name,
-      email: req.body.email.toLowerCase().trim(),
-      password: req.body.password
-  };
-  if (data.name && data.email && data.password) {
-      var salt = bcrypt.genSaltSync(10);
-      var hash = bcrypt.hashSync(data.password, salt);
-      data['password'] = hash;
-      Users.create(data).then(function (user) {
-          res.setHeader('Content-Type', 'application/json');
-          res.end(JSON.stringify(user));
-      });;
-  } else {
-      res.status(434).send('Name, email and password is required to register')
-  }
+});
+
+app.post('/api/register', function (req, res) {
+    
+    let data = {
+        
+        first_name: req.body.first_name.trim(),
+        last_name: req.body.last_name.trim(),
+        email: req.body.email.toLowerCase().trim(),
+        password: req.body.password
+
+    };
+    
+    if (data.first_name && data.last_name && data.email && data.password) {
+        
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync(data.password, salt);
+        data['password_hash'] = hash;
+
+        Users.create(data).then(function (user) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(user));
+        });
+
+    } else {
+        
+        res.status(434).send('First and Last name, email and password is required to register')
+    
+    }
+
 });
 
 app.listen(3000);
